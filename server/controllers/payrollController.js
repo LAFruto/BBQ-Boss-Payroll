fb7ea8db0aa600056afd00d05dee15cbd67635c6
JayElDeeYunno <118359037+JayElDeeYunno@users.noreply.github.com>
2024-03-15 17:42:08 +0800

    hurr hurr

--- server/controllers/payrollController.js
+++ server/controllers/payrollController.js
@@ -15,6 +15,34 @@ exports.view = (req, res) => {
     res.render("payroll");
 };
 
+async function fetchSummary(period_id, emp_id) {
+    // get active emp dtrs under the current period
+    const dtrQuery = `
+      SELECT 
+        dtr.id AS dtr_id,
+        dtr.emp_id,
+        dtr.date_id,
+        d.date,
+        dtr.start_time, 
+        dtr.end_time,
+        dtr.status_id,
+        dtr.hasot,
+        dtr.hasbreak,
+        d.day_type_id,
+        dtr.branch_id,
+      FROM 
+        tbl_daily_time_records dtr
+      JOIN
+        tbl_dates d ON dtr.date_id = d.id
+      WHERE
+        d.date BETWEEN $1 AND $2
+      ORDER BY
+        dtr.emp_id,
+        d.date;
+    `;
+
+}
+
 exports.form = (req, res) => {
     res.render('convert-mbos');
     
